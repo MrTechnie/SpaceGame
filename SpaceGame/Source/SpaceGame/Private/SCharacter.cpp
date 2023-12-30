d159417d71a892f552135eccac3bc1113bfcde61
MrTechnie <137833610+MrTechnie@users.noreply.github.com>
2023-12-30 13:06:02 -0500

    Smaller files
    File breakdown1

---
+++ SpaceGame/Source/SpaceGame/Private/SCharacter.cpp
@@ -0,0 +1,138 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SCharacter.h"
+#include <Camera/CameraComponent.h>
+#include <GameFramework/SpringArmComponent.h>
+#include <GameFramework/CharacterMovementComponent.h>
+#include <Components/InputComponent.h>
+
+// Sets default values
+ASCharacter::ASCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->SetupAttachment(SpringArmComp);
+
+	SpringArmComp->bUsePawnControlRotation = true;
+
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationPitch = false;
+
+
+	//Jump Properties
+
+	GetCharacterMovement()->JumpOffJumpZFactor = 100.0f;
+	GetCharacterMovement()->JumpZVelocity = 500.0f;
+	GetCharacterMovement()->AirControl = 50.0f;
+
+	//Crouch Properties
+	
+	//bCrouchCooldown = false;
+	float CrouchCooldownDuration = 1.0f;
+	GetCharacterMovement()->MaxWalkSpeedCrouched = 200.0f;
+	GetCharacterMovement()->CrouchedHalfHeight = 60.0f;
+	
+}
+
+// Called when the game starts or when spawned
+void ASCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASCharacter::MoveForward(float value)
+{
+	AddMovementInput(GetActorForwardVector(), value);
+}
+
+void ASCharacter::MoveRight(float value)
+
+{
+	AddMovementInput(GetActorRightVector(), value);
+
+
+}
+
+
+void ASCharacter::Jump()
+{
+
+	Super::Jump();
+
+
+}
+
+//void ASCharacter::CrouchDelay()
+//{
+	
+	//bCrouchCooldown = true;
+	
+	//GetWorld()->GetTimerManager().SetTimer(CoolDownTimerHandle, this, &ASCharacter::ResetCrouchDelay, CrouchCooldownDuration, false);
+
+
+
+//}
+
+//void ASCharacter::ResetCrouchDelay()
+//{
+	//bCrouchCooldown = false;
+//}
+
+void ASCharacter::Crouch()
+
+{
+	//if (bCrouchCooldown == false) {
+
+		Super::Crouch();
+	//	CrouchDelay();
+	//}
+
+	//nothing should happen if ShouldWait is true, as the function should not be executed.
+
+}
+
+void ASCharacter::UnCrouch()
+{
+	
+	//if (bCrouchCooldown == false) {
+
+		Super::UnCrouch();
+		//CrouchDelay();
+	//}
+
+
+	//nothing should happen if ShouldWait is true, as the function should not be executed.
+}
+
+
+// Called every frame
+void ASCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//Camera Control
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("LookSide", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	
+	//Locomotion
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ASCharacter::Jump);
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ASCharacter::Crouch);
+	PlayerInputComponent->BindAction("UnCrouch", IE_Released, this, &ASCharacter::UnCrouch);
+}
+
---
+++ SpaceGame/Source/SpaceGame/Public/SCharacter.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class SPACEGAME_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASCharacter();
+	
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//Basic Movement
+	void MoveForward(float value);
+	void MoveRight(float value);
+	void Jump();
+
+	//Crouch Functions
+   void Crouch();
+	void UnCrouch();
+	//void CrouchDelay();
+	//void ResetCrouchDelay(); 
+
+	//Crouch Member Variables
+
+//	FTimerHandle CoolDownTimerHandle;
+//	bool bCrouchCooldown; 
+//	float CrouchCooldownDuration;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
