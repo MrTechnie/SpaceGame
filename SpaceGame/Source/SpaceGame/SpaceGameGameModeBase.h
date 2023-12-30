696fe10cf3658c0d90d2c6821f5d38f730681dde
MrTechnie <137833610+MrTechnie@users.noreply.github.com>
2023-12-30 13:05:16 -0500

    Character Improvements
    Character movement

---
+++ SpaceGame/Source/SpaceGame/SpaceGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpaceGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEGAME_API ASpaceGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
