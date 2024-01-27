89e261837153364255ab8d7ea20e55f748a5b8c7
MrTechnie <137833610+MrTechnie@users.noreply.github.com>
2024-01-27 15:55:15 -0500

    files

--- SpaceGame/Intermediate/Build/Win64/UnrealEditor/Inc/SpaceGame/UHT/SInventory_AC.generated.h
+++ SpaceGame/Intermediate/Build/Win64/UnrealEditor/Inc/SpaceGame/UHT/SInventory_AC.generated.h
@@ -14,11 +14,11 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define SPACEGAME_SInventory_AC_generated_h
 
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_SPARSE_DATA
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_RPC_WRAPPERS
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_RPC_WRAPPERS_NO_PURE_DECLS
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_ACCESSORS
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_INCLASS_NO_PURE_DECLS \
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_SPARSE_DATA
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_RPC_WRAPPERS
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_ACCESSORS
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesUSInventory_AC(); \
 	friend struct Z_Construct_UClass_USInventory_AC_Statics; \
@@ -27,7 +27,7 @@ public: \
 	DECLARE_SERIALIZER(USInventory_AC)
 
 
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_INCLASS \
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_INCLASS \
 private: \
 	static void StaticRegisterNativesUSInventory_AC(); \
 	friend struct Z_Construct_UClass_USInventory_AC_Statics; \
@@ -36,7 +36,7 @@ public: \
 	DECLARE_SERIALIZER(USInventory_AC)
 
 
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_STANDARD_CONSTRUCTORS \
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API USInventory_AC(const FObjectInitializer& ObjectInitializer); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USInventory_AC) \
@@ -50,7 +50,7 @@ public: \
 	NO_API virtual ~USInventory_AC();
 
 
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_ENHANCED_CONSTRUCTORS \
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_ENHANCED_CONSTRUCTORS \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	NO_API USInventory_AC(USInventory_AC&&); \
@@ -62,27 +62,27 @@ public: \
 	NO_API virtual ~USInventory_AC();
 
 
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_10_PROLOG
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_GENERATED_BODY_LEGACY \
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_11_PROLOG
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_SPARSE_DATA \
-	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_RPC_WRAPPERS \
-	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_ACCESSORS \
-	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_INCLASS \
-	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_STANDARD_CONSTRUCTORS \
+	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_SPARSE_DATA \
+	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_RPC_WRAPPERS \
+	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_ACCESSORS \
+	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_INCLASS \
+	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_GENERATED_BODY \
+#define FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_SPARSE_DATA \
-	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
-	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_ACCESSORS \
-	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_INCLASS_NO_PURE_DECLS \
-	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_13_ENHANCED_CONSTRUCTORS \
+	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_SPARSE_DATA \
+	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_ACCESSORS \
+	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_INCLASS_NO_PURE_DECLS \
+	FID_SpaceGame_Source_SpaceGame_Public_SInventory_AC_h_14_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
