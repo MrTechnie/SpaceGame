498377c870ef89e7f54a778088be7c3e5cc199ce
MrTechnie <137833610+MrTechnie@users.noreply.github.com>
2024-01-26 19:46:47 -0500

    files

--- SpaceGame/Intermediate/Build/Win64/UnrealEditor/Inc/SpaceGame/UHT/SpaceGame.init.gen.cpp
+++ SpaceGame/Intermediate/Build/Win64/UnrealEditor/Inc/SpaceGame/UHT/SpaceGame.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeSpaceGame_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x6CF058F6,
-				0x64B9196B,
+				0x07A05AE6,
+				0xF8D9BAF4,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_SpaceGame.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_SpaceGame.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SpaceGame(Z_Construct_UPackage__Script_SpaceGame, TEXT("/Script/SpaceGame"), Z_Registration_Info_UPackage__Script_SpaceGame, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x6CF058F6, 0x64B9196B));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_SpaceGame(Z_Construct_UPackage__Script_SpaceGame, TEXT("/Script/SpaceGame"), Z_Registration_Info_UPackage__Script_SpaceGame, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x07A05AE6, 0xF8D9BAF4));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
