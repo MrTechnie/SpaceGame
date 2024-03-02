e823b13695574f21768728a1ae4fb0ace0c20d3e
MrTechnie <137833610+MrTechnie@users.noreply.github.com>
2024-03-02 16:43:37 -0500

    files

---
+++ SpaceGame/Source/SpaceGame/Public/W_Inventory.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "W_Inventory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEGAME_API AW_Inventory : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+
+	 
+};
